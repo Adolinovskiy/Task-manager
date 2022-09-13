f84f0898478cb4526ae74a02e0b0f94d3ba39235
adolinovskiy <adolinovskiybackend@gmail.com>
2022-09-14 02:36:37 +0300

    first commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        // useFindAndModify: false
+    });
+}
+
